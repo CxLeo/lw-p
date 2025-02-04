4d46721a6aa8cb527e3e3ad7fb95c5d83ff9223c
cxLeoo <leo@noqclinic.com>
2025-02-04 13:51:48 -0500

    refined background grid

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="w-screen lg:px-[60px] xl:px-[150px]">
+    <main className="w-screen lg:px-[60px] xl:px-[150px] px-[25px]">
       {/* background grid */}
-      <div className="absolute lg:px-[60px] xl:px-[150px] h-screen w-full top-0 left-0 grid grid-flow-row lg:grid-cols-4 grid-cols-1 gap-4">
-        <div className="h-full border-l border-r border-dashed border-[#0000001A]" />
-        <div className="h-full border-l border-r border-dashed border-[#0000001A]" />
-        <div className="h-full border-l border-r border-dashed border-[#0000001A]" />
-        <div className="h-full border-l border-r border-dashed border-[#0000001A]" />
+      <div className="absolute px-[25px] lg:px-[60px] xl:px-[150px] h-[600vh] -mt-2 w-full top-0 left-0 grid grid-flow-row lg:grid-cols-4 grid-cols-1 gap-4 overflow-y-hidden">
+        <div className="custom-dashed-border h-full "></div>
+        <div className="custom-dashed-border h-full "></div>
+        <div className="custom-dashed-border h-full "></div>
+        <div className="custom-dashed-border h-full "></div>
       </div>
-      {/* content */}
+      {/* Content */}
       <div className="relative">
         <Hero/>
       </div>
